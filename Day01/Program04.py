c898c7d40109e250c346dde9f06bd76bf76ecb89
Pranay1906 <65688346+Pranay1906@users.noreply.github.com>
2020-05-21 05:32:47 +0530

    Create Program04.py

---
+++ Day01/Program04.py
@@ -0,0 +1,5 @@
+c_price= int(input("Entert a cost price"))
+s_price= int(input("Entert a sell price"))
+profit =(s_price-c_price)
+print(f"profit = {profit}")
+print(f"seeling price should be :{s_price+profit*(5/100)} for 5 % more profit")
