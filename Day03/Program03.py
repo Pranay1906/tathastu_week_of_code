151a9f82e5fdce7d370dd361f8dfb61087e549fd
Pranay1906 <65688346+Pranay1906@users.noreply.github.com>
2020-05-30 16:53:10 +0530

    Update Program03.py

--- Day03/Program03.py
+++ Day03/Program03.py
@@ -1,17 +1,8 @@
-def String(List):
-    return ''.join(List)
-def Permute(a, l, r):
-    if l == r:
-        print(String(a))
-    else:
-        for i in range(l, r + 1):
-            a[l], a[i] = a[i], a[l]
-            Permute(a, l + 1, r)
-            a[l], a[i] = a[i], a[l]
+z = input(('Enter a String:'))
+x=list(z)
+for i in range(len(x)):
+    for j in range(i+1, len(x)):
+        if x[i] == x[j]:
+            x[i]=""
 
-
-
-string = "ABC"
-n = len(string)
-a = list(string)
-Permute(a, 0, n - 1)
+print(''.join(x))
